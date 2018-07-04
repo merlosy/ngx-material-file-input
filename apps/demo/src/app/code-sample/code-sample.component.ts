49f379929c318683ec74b3e2ca60bd7e210f2c55
merlosy <jeremy.legros85@gmail.com>
2018-07-04 23:27:40 +0200

    fix linter

--- apps/demo/src/app/code-sample/code-sample.component.ts
+++ apps/demo/src/app/code-sample/code-sample.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  OnInit,
-  Input,
-  ChangeDetectionStrategy
-} from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-code-sample',
