a6c14108a5ba2df6e5142e46d8d20d31c1ada1b2
merlosy <jeremy.legros85@gmail.com>
2019-01-12 12:42:00 -0500

    export FileInputConfig

--- libs/material-file-input/src/index.ts
+++ libs/material-file-input/src/index.ts
@@ -1,6 +1,7 @@
 export { NGX_MAT_FILE_INPUT_CONFIG } from './lib/model/file-input-config.model';
 export { MaterialFileInputModule } from './lib/material-file-input.module';
 export { FileInputComponent } from './lib/file-input/file-input.component';
+export { FileInputConfig } from './lib/model/file-input-config.model';
 export { ByteFormatPipe } from './lib/pipe/byte-format.pipe';
 export { FileValidator } from './lib/validator/file-validator';
 export { FileInput } from './lib/model/file-input.model';
