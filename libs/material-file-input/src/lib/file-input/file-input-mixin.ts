999de538f36f34ef6c119c71df093f62cda1109c
Aaron Moore <Amoore@mainstream-tech.com>
2021-11-08 11:52:10 -0600

    updates to ng13 and version bump

--- libs/material-file-input/src/lib/file-input/file-input-mixin.ts
+++ libs/material-file-input/src/lib/file-input/file-input-mixin.ts
@@ -1,19 +1,18 @@
 import { FormGroupDirective, NgControl, NgForm } from '@angular/forms';
-import { CanUpdateErrorStateCtor, ErrorStateMatcher, mixinErrorState } from '@angular/material/core';
+import { ErrorStateMatcher, mixinErrorState } from '@angular/material/core';
 
 // Boilerplate for applying mixins to FileInput
 /** @docs-private */
 export class FileInputBase {
-  constructor(public _defaultErrorStateMatcher: ErrorStateMatcher,
-      public _parentForm: NgForm,
-      public _parentFormGroup: FormGroupDirective,
-      public ngControl: NgControl) { }
+  constructor(
+    public _defaultErrorStateMatcher: ErrorStateMatcher,
+    public _parentForm: NgForm,
+    public _parentFormGroup: FormGroupDirective,
+    public ngControl: NgControl
+  ) {}
 }
 
 /**
  * Allows to use a custom ErrorStateMatcher with the file-input component
  */
-export const FileInputMixinBase:
-  CanUpdateErrorStateCtor &
-  typeof FileInputBase =
-  mixinErrorState(FileInputBase);
+export const FileInputMixinBase = mixinErrorState(FileInputBase);
