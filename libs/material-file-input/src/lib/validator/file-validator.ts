9047ec3081424588d50ddcb80d6e30cbe5d47758
merlosy <jeremy.legros85@gmail.com>
2021-11-21 16:29:49 -0500

    fix: material-file-input lib tests

--- libs/material-file-input/src/lib/validator/file-validator.ts
+++ libs/material-file-input/src/lib/validator/file-validator.ts
@@ -1,7 +1,7 @@
 import { ValidatorFn, AbstractControl } from '@angular/forms';
 import { FileInput } from '../model/file-input.model';
 
-export namespace FileValidator {
+export class FileValidator {
   /**
    * Function to control content of files
    *
@@ -9,7 +9,7 @@ export namespace FileValidator {
    *
    * @returns
    */
-  export function maxContentSize(bytes: number): ValidatorFn {
+  static maxContentSize(bytes: number): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
       const size = control && control.value ? (control.value as FileInput).files.map(f => f.size).reduce((acc, i) => acc + i, 0) : 0;
       const condition = bytes >= size;
