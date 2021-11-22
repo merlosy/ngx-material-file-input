e30212b9f8eba24990c2973f2044de47fb4101af
merlosy <jeremy.legros85@gmail.com>
2021-11-21 20:46:53 -0500

    test: tweak config

--- libs/material-file-input/karma.conf.js
+++ libs/material-file-input/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
+      dir: require('path').join(__dirname, '../../coverage/libs/material-file-input'),
       subdir: '.',
       reporters: [
         { type: 'html' },
