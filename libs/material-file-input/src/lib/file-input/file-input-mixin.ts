0aac6d656ca375fe23aa042f602dabedb7b43943
Nikhil Sharma <nikhil.sharma@tietoevry.com>
2022-06-14 15:39:06 +0530

    Compatibility with Angular 14 (https://github.com/merlosy/ngx-material-file-input/issues/111) -Upgrading dependencies -Adding stateChanges property to support changes to MatFormFieldControl in v14

--- libs/material-file-input/src/lib/file-input/file-input-mixin.ts
+++ libs/material-file-input/src/lib/file-input/file-input-mixin.ts
@@ -1,5 +1,6 @@
 import { FormGroupDirective, NgControl, NgForm } from '@angular/forms';
-import { ErrorStateMatcher, mixinErrorState } from '@angular/material/core';
+import {ErrorStateMatcher, mixinErrorState} from '@angular/material/core';
+import {Subject} from "rxjs";
 
 // Boilerplate for applying mixins to FileInput
 /** @docs-private */
@@ -8,7 +9,8 @@ export class FileInputBase {
     public _defaultErrorStateMatcher: ErrorStateMatcher,
     public _parentForm: NgForm,
     public _parentFormGroup: FormGroupDirective,
-    public ngControl: NgControl
+    public ngControl: NgControl,
+    public stateChanges: Subject<void>
   ) {}
 }
 
